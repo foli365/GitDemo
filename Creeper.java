74dcd97b731434c428081f5e4f1029b761fbff0e
Yves Pan <foli365@gmail.com>
2021-08-15 16:19:03 +0800

    add modelheight

--- Creeper.java
+++ Creeper.java
@@ -3,5 +3,5 @@ import java.util.ArrayList
 public class Creeper{
 	public static int HP = 1000;
 	public static int BlastDamage = 500;
-	public static int BlastRadius = 16; 
+	public static int Modelheight = 3;
 }
