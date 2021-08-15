20f6691f8c8e82b120abe582031d28ea7443ce82
Yves Pan <78410711+foli365@users.noreply.github.com>
2021-08-15 16:48:29 +0800

    Charged condition added
     triggered if strike by lightening

--- Creeper.java
+++ Creeper.java
@@ -1,8 +1,10 @@
-import java.util.ArrayList
-
 public class Creeper{
 	public static int HP = 1000;
-	public static int BlastDamage = 500;
+	public static double BlastDamage = 500.0;
 	public static int BlastRadius = 6; 
 	public static int Modelheight = 3;
+	public boolean charged = true;
+	if(charged) {
+		BlastDamage * 2.71
+	}
 }
